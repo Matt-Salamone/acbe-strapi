957aa635e9364957246e3c1f3b9f3326c4ac3920
Matt-Salamone <175448127+Matt-Salamone@users.noreply.github.com>
2025-06-01 12:35:23 -0400

    feat: add test API with controller, service, and routes; implement database configuration for production and development

--- config/database.ts
+++ config/database.ts
@@ -1,16 +1,32 @@
 const path = require('path');
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'postgres',
-    connection: {
-      connectionString: env('DATABASE_URL'),
-      ssl: env.bool('DATABASE_SSL', false),
-    },
-    acquireConnectionTimeout: 60000,
-    pool: {
-      min: 2,
-      max: 10,
-    },
-  },
-});
+module.exports = ({ env }) => {
+  if (env('NODE_ENV') === 'production') {
+    // Production configuration (PostgreSQL)
+    return {
+      connection: {
+        client: 'postgres',
+        connection: {
+          connectionString: env('DATABASE_URL'),
+          ssl: env.bool('DATABASE_SSL', false),
+        },
+        acquireConnectionTimeout: 60000,
+        pool: {
+          min: 2,
+          max: 10,
+        },
+      },
+    };
+  } else {
+    // Development configuration (SQLite)
+    return {
+      connection: {
+        client: 'sqlite',
+        connection: {
+          filename: path.join(__dirname, '..', '..', '.tmp/data.db'),
+        },
+        useNullAsDefault: true,
+      },
+    };
+  }
+};
