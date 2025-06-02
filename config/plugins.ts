99a52bbb85a06a8ac11c50506ec1e8e86f852e4e
Matt-Salamone <175448127+Matt-Salamone@users.noreply.github.com>
2025-06-02 16:50:21 -0400

    feat: add documentation plugin to project dependencies and configure documentation settings

--- config/plugins.ts
+++ config/plugins.ts
@@ -4,4 +4,28 @@ module.exports = ({ env }) => ({
       jwtSecret: env('JWT_SECRET'),
     },
   },
+  documentation: {
+    enabled: true,
+    config: {
+      openapi: '3.0.0',
+      info: {
+        version: '1.0.0',
+        title: 'DOCUMENTATION',
+        description: '',
+        termsOfService: 'YOUR_TERMS_OF_SERVICE_URL',
+        contact: {
+          name: 'TEAM',
+          email: 'contact-email@company.com',
+          url: 'mywebsite.io'
+        },
+        license: {
+          name: 'Apache 2.0',
+          url: 'https://www.apache.org/licenses/LICENSE-2.0.html'
+        },
+      },
+      'x-strapi-config': {
+        plugins: ['upload', 'users-permissions'],
+      },
+    },
+  },
 });
